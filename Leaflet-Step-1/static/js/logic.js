2f8f443635def09c3ee7e0fed31482e95ed01a5a
maylacdao <may.lacdao@gmail.com>
2021-02-20 19:25:48 -0500

    Update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,4 +1,4 @@
-const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 d3.json(url, function(data) {
     console.log(data.features);
@@ -11,18 +11,25 @@ function createFeatures(earthquakeData) {
         layer.bindPopup(feature.properties.place + "<hr />" + new Date(feature.properties.time));
     }
 
-    // function chooseColor(depth) {
-    //     switch        
-    // }
+    function getColor(d) {
+    return d <= 10 ? '#fcc5c0' :
+           d <= 30  ? '#fa9fb5' :
+           d <= 50  ? '#f768a1' :
+           d <= 70  ? '#dd3497' :
+           d <= 90   ? '#ae017e' :
+           d > 90   ? '#7a0177' :
+                      '#49006a';
+    }
 
     var earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
         pointToLayer: function(feature, latlng) {
             var circle = L.circle(latlng, {
                 radius: feature.properties.mag * 15000,
-                color: "#2dc4b2",
-                fillOpacity: 0.75
-                // fillColor: chooseColor(feature.geometry.coordinates[3])
+                color: getColor(feature.geometry.coordinates[2]),
+                weight: 0,
+                fillOpacity: 0.50,
+                fillColor: getColor(feature.geometry.coordinates[2])
             });
                 return circle;
         }
@@ -42,8 +49,18 @@ function createMap(earthquakes) {
         accessToken: API_KEY
     });
 
+    var darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/dark-v10',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: API_KEY
+    });
+
     var baseMaps = {
-        "Light Map": lightmap
+        "Light Map": lightmap,
+        "Dark Map": darkmap
     };
 
     var overlayMaps = {
@@ -51,12 +68,45 @@ function createMap(earthquakes) {
     };
 
     var map = L.map("mapid", {
-        center: [35.5858333, -117.5511667],
-        zoom: 5,
-        layers: [lightmap, earthquakes]
+        center: [0,0],
+        zoom: 2,
+        layers: [darkmap, earthquakes]
     });
+
+    var legend = L.control({
+        position: "bottomright"
+    });
+
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "legend");
+
+        var depths = [-10, 10, 30, 50, 70, 90];
+        var colors = [
+            '#fcc5c0',
+            '#fa9fb5',
+            '#f768a1',
+            '#dd3497',
+            '#ae017e',
+            '#7a0177']
+    
+
+    for (var i = 0; i < depths.length; i++) {
+        div.innerHTML += "<i style='background: "
+            + colors[i]
+            + " '></i>"
+            + depths[i]
+            + (depths[i + 1] ? "&ndash;" 
+            + depths[i + 1] 
+            + "<br />" : "+");
+    }
+    return div;
+    };
+
+    legend.addTo(map);
 
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(map);
+
+    
 }
